40f633f14f9bc050f15cd71ca056e9436cd05201
AYusufKuru <ahmetyusuf.kuru@gmail.com>
2024-11-18 23:38:29 +0300

    a

--- frontend/src/components/modals/WorkerAddModal.jsx
+++ frontend/src/components/modals/WorkerAddModal.jsx
@@ -141,6 +141,15 @@ const WorkerAddModal = () => {
     onSubmit: submit,
   })
 
+  const [files, setFiles] = useState([])
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFiles((prevFiles) => [...prevFiles, file])
+    }
+  }
+
   return (
     <div className="p-5 h-full overflow-scroll z-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
       <form onSubmit={handleSubmit} encType="multipart/form-data" className="bg-lightGray rounded-lg shadow-lg  w-[1400px] p-8">
@@ -1197,99 +1206,34 @@ const WorkerAddModal = () => {
             <div className="p-4 bg-white border-t border-gray-300">
                 <div className="">
                     <div className='flex justify-end w-full'>
-                        <button className='text-white text-lg font-semibold bg-sky-600 hover:bg-sky-500 py-3 px-5 rounded-xl '>Dosya Ekle</button>
+                        <label htmlFor='file-input' className='text-white text-lg font-semibold bg-sky-600 hover:bg-sky-500 py-3 px-5 rounded-xl'>
+                           <input
+                                id="file-input"
+                                type="file"
+                                className="hidden"
+                                onChange={handleFileChange}
+                            />
+                            Dosya Ekle
+                        </label>
                     </div>
 
                     <div className="grid grid-cols-3 gap-4 mt-5">
-                        {/* İlk Dosya */}
-                        <div className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                            <polyline points="10 9 9 9 8 9" />
-                        </svg>
-                        <div>
-                            <p className="text-gray-700 font-semibold">Dosya-1.pdf</p>
-                            <p className="text-sm text-gray-500">1.2 MB</p>
-                        </div>
-                        </div>
-
-                        {/* İkinci Dosya */}
-                        <div className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-green-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                            <polyline points="10 9 9 9 8 9" />
-                        </svg>
-                        <div>
-                            <p className="text-gray-700 font-semibold">Dosya-2.pdf</p>
-                            <p className="text-sm text-gray-500">3.5 MB</p>
-                        </div>
-                        </div>
-
-                        {/* Üçüncü Dosya */}
-                        <div className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                            <polyline points="10 9 9 9 8 9" />
-                        </svg>
-                        <div>
-                            <p className="text-gray-700 font-semibold">Dosya-3.pdf</p>
-                            <p className="text-sm text-gray-500">500 KB</p>
-                        </div>
-                        </div>
-
-                        {/* Üçüncü Dosya */}
-                        <div className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                            <polyline points="10 9 9 9 8 9" />
-                        </svg>
-                        <div>
-                            <p className="text-gray-700 font-semibold">Dosya-4.pdf</p>
-                            <p className="text-sm text-gray-500">134 KB</p>
-                        </div>
-                        </div>
-                        {/* Üçüncü Dosya */}
-                        <div className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-sky-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                            <polyline points="10 9 9 9 8 9" />
-                        </svg>
-                        <div>
-                            <p className="text-gray-700 font-semibold">Dosya-5.pdf</p>
-                            <p className="text-sm text-gray-500">45.3 KB</p>
-                        </div>
-                        </div>
-                        {/* Üçüncü Dosya */}
-                        <div className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-green-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <polyline points="14 2 14 8 20 8" />
-                            <line x1="16" y1="13" x2="8" y2="13" />
-                            <line x1="16" y1="17" x2="8" y2="17" />
-                            <polyline points="10 9 9 9 8 9" />
-                        </svg>
-                        <div>
-                            <p className="text-gray-700 font-semibold">Dosya-6.pdf</p>
-                            <p className="text-sm text-gray-500">430 KB</p>
-                        </div>
-                        </div>
-                </div>
-
+                        {files.map((file, index) => (
+                            <div key={index} className="border rounded-lg p-4 flex items-center space-x-4 shadow-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-blue-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+                                <polyline points="14 2 14 8 20 8" />
+                                <line x1="16" y1="13" x2="8" y2="13" />
+                                <line x1="16" y1="17" x2="8" y2="17" />
+                                <polyline points="10 9 9 9 8 9" />
+                            </svg>
+                            <div>
+                                <p className="text-gray-700 font-semibold">{file.name}</p>
+                                <p className="text-sm text-gray-500">{(file.size / (1024 * 1024)).toFixed(2)} MB</p>
+                            </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
             )}
