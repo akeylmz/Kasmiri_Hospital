fdcf76f967fa61f8e1c8ef6ee4df342caa2887ae
AYusufKuru <ahmetyusuf.kuru@gmail.com>
2024-12-25 03:41:19 +0300

    commit

---
+++ frontend/src/ProtectedRoute.jsx
@@ -0,0 +1,49 @@
+
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom'
+import { useRefreshTokenMutation } from './store/patient2';
+import { useEffect } from 'react';
+import { setAuth } from './store/authSlice';
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+    const dispatch = useDispatch();
+  const { token, refreshToken  } = useSelector(state => state.auth);
+  const role = useSelector(state => state.auth.role);
+  const [refreshTokenRequest, { isLoading, error }] = useRefreshTokenMutation();
+  
+  useEffect(() => {
+    const getNewAccessToken = async () => {
+      if (!refreshToken) return;
+
+      try {
+        const response = await refreshTokenRequest(refreshToken).unwrap();
+        
+        if (response) {
+            console.log("Yeni token:", response);
+          dispatch(setAuth({
+            token: response.access,
+            refreshToken: response.refresh,
+            user: 'user_name', 
+            role: 'user_role',
+          }));
+
+          localStorage.setItem('accessToken', response.access);
+          localStorage.setItem('refreshToken', response.refresh);
+        }
+      } catch (error) {
+        console.error('Yeni token alınırken hata oluştu:', error);
+      }
+    };
+
+    if (!token && refreshToken) {
+      getNewAccessToken();
+    }
+  }, [token, refreshToken, dispatch, refreshTokenRequest]);
+
+  if (!token && !refreshToken) return <Navigate to="/login" />
+//   if (!role || (allowedRoles && !allowedRoles.includes(role)))  return <Navigate to="/unauthorized" />
+ 
+  return children
+}
+
+export default ProtectedRoute;
